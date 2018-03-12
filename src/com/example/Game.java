b59e70b2d76ca4ac4c24238366aa794c95825f33
noletubby77 <noletubby77@hotmail.com>
2018-03-12 07:45:15 -0400

    winning logic working, game not stopping

--- src/com/example/Game.java
+++ src/com/example/Game.java
@@ -97,20 +97,20 @@ public class Game {
 
             if (counter % 2 == 0) {
                 System.out.println(player1 + " enter row (1-3)");
-                row = input.nextInt();
+                col = input.nextInt();
                 System.out.println(player1 + " enter column (1-3)");
 
-                col = input.nextInt();
+                row = input.nextInt();
                 setBoard(row - 1, col - 1, 'x');
 //                printBoard();
 
 //                System.out.println("counter is" + counter);
             } else {
                 System.out.println(player2 + " enter row (1-3)");
-                row = input.nextInt();
+                col = input.nextInt();
                 System.out.println(player2 + " enter column (1-3)");
 
-                col = input.nextInt();
+                row = input.nextInt();
                 setBoard(row - 1, col - 1, 'o');
                 printBoard();
 
@@ -134,32 +134,45 @@ public class Game {
             System.out.println("It's a draw! No winner :(");
         } else {
             for (int i = 0; i < 3; i++) {
-//                System.out.println(gameboard[i][0]);
-//                System.out.println(gameboard[i][2]);
-//                System.out.println(gameboard[i][1]);
+//
                 // check the rows to see if there are matching values in each spot
                 if (gameboard[i][0] == gameboard[i][1] && gameboard[i][1] == gameboard[i][2] && gameboard[i][0] != Open) {
                     keepPlaying = false;
                     displayWinner();
+                }
 
-                } else if (gameboard[0][i] == gameboard[1][i] && gameboard[1][i] == gameboard[2][i] && gameboard[0][i] != Open) {
+                // check the colums to see if there are matching values in each spot
+                 else if (gameboard[0][i] == gameboard[1][i] && gameboard[1][i] == gameboard[2][i] && gameboard[0][i] != Open) {
                     keepPlaying = false;
                     displayWinner();
-                } else if (gameboard[0][0] == gameboard[1][1] && gameboard[1][1] == gameboard[2][2] && gameboard[0][0] != Open)
+
+                }
+
+                // check diagonals for matching values
+                else if (gameboard[0][0] == gameboard[1][1] && gameboard[1][1] == gameboard[2][2] && gameboard[0][0] != Open) {
                     keepPlaying = false;
-                displayWinner();
-            } else
-            if (gameboard[1][3] == gameboard[2][2] && gameboard[2][2] == gameboard[3][1] && gameboard[1][3] != Open)
-                keepPlaying = false;
-            displayWinner();
+                    displayWinner();
+                }
+
+                // check other diagonal for matching values
+                else if (gameboard[0][2] == gameboard[1][1] && gameboard[2][0] == gameboard[2][0] && gameboard[0][2] != Open) {
+                    keepPlaying = false;
+                    displayWinner();
+                } else keepPlaying = true;
+            }
         }
     }
 
 
 
     public void displayWinner() {
-        if (counter % 2 == 0) {
+        // check counter to determine the current player
+        // since the counter will already have been adjusted after a valid move, we need to check for opposites
+        if (counter % 2 != 0) {
             System.out.println("Congratulations " + player1 + "!. You WON!!");
+        } else {
+            System.out.println("Congratulations " + player2 + "!. You WON!!");
+
         }
     }
         // https://www.mkyong.com/java/how-to-convert-character-to-ascii-in-java/
