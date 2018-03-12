e34e67031fae02c182505d92ff98ac5fcb3792d1
noletubby77 <noletubby77@hotmail.com>
2018-03-11 22:38:59 -0400

    get user names and start game added

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,12 +1,42 @@
 package com.example;
+import java.util.Scanner;
+
 
 public class Main {
+    public int counter = 0;
+    static Scanner input = new Scanner(System.in);
+    public static String player1;
+    public static String player2;
+    public static String start;
+
+
 
     public static void main(String[] args) {
         Game newGame = new Game();
 
+        System.out.println("Player X, Enter Your Name:");
+
+        player1 = input.next();
+        System.out.println("Player O, Enter Your Name:");
+        player2 = input.next();
+        System.out.println("Welcome " + player1 + " and " + player2 + "!");
+        System.out.println("Start game? y/n");
+        start = input.next();
+        if (start.equalsIgnoreCase("y")) {
+            System.out.println("Start");
+        }
+
+
+
+
         newGame.gameStart();
         newGame.printBoard();
-        }
+
+        input.close();
+
     }
-//}
+}
+
+
+
+
